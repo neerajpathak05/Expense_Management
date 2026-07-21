2112275e83f43045ad0d797704c5de74326d5f9c
Neeraj Pathak <your_email@example.com>
2026-07-21 09:38:56 +0530

    first-commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://expense-management-backend-inm4.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
